30cb743828aed4a3f869e36b5954bf5af53c31fc
root <root@b6b4cb6241ef>
2017-08-04 20:02:24 +0000

    Got into the twitter api, request are working, one bug that needs to be fixed

--- src/client/api.js
+++ src/client/api.js
@@ -1,15 +1,40 @@
+const URL = require('url');
+const helper = require('../util/helpers');
+const crypto = require('crypto');
+const snekfetch = require('snekfetch');
+
 exports.API = class {
     constructor(client) {
         this.client = client;
     }
     
-    async request(method, url, options = {}) {
-        method = method.toUpperCase();
-        const config = this.client.credentials;
-        const signkey = `${encodeURIComponent(config.consumer_secret)}&${encodeURIComponent(config.access_token_secret)}`;
-        const paramstring = `${options.s}`
-        const basestring = `${method}&${url}&`
-        
-        
+    async request(method, url) {
+        return new Promise((resolve, reject) => {
+            const parsedUrl = URL.parse(url, false);
+            let options = {
+                oauth_consumer_key: this.client.credentials.consumer_key,
+                oauth_nonce: helper.genNonce(32),
+                oauth_signature_method: 'HMAC-SHA1',
+                oauth_timestamp: Math.floor( (new Date()).getTime() / 1000 ).toString(),
+                oauth_token: this.client.credentials.access_token,
+                oauth_version: '1.0A'
+            };
+            options = helper.sortObject(options);
+            const paramval = helper.fixedEncodeURIComponent(helper.stringfiller('param', options));
+            const url = helper.fixedEncodeURIComponent(url);
+            const basesign = `${method.toUpperCase()}&${url}&${paramval}`;
+            const signkey = `${helper.fixedEncodeURIComponent(this.client.credentials.consumer_secret)}&${helper.fixedEncodeURIComponent(this.client.credentials.access_token_secret)}`;
+            options.oauth_signature = crypto.createHmac('sha1', signkey).update(basesign).digest('base64');
+            const headerval = helper.stringfiller('oauthheader', options);
+            const resp = await snekfetch.get('https://api.twitter.com/1.1/account/verify_credentials.json')
+                .set('Accept', '*/*')
+                .set(`Authorization`, headerval)
+                .set('Connection', 'close')
+                .set('Content-Type', 'application/x-www-form-urlencoded')
+                .set('Host', parsedUrl.host)
+                .set('Content-length', 0)
+                .catch(e => reject(e));
+            resolve(resp);
+        });
     }
-}
+};
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,15 +1,25 @@
-exports.randomString = (length) => {
-    let text = "";
-    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for(var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
+exports.NONCE_CHARS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+              'o','p','q','r','s','t','u','v','w','x','y','z','A','B',
+              'C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+              'Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3',
+              '4','5','6','7','8','9'];
 
-exports.fixedEncodeURIComponent = (str) => {
-  return encodeURIComponent(str).replace(/[!'()*]/g, (c) => '%' + c.charCodeAt(0).toString(16));
-}
+exports.genNonce = (nonceSize) => {
+   var result = [];
+   var chars= this.NONCE_CHARS;
+   var char_pos;
+   var nonce_chars_length= chars.length;
+
+   for (var i = 0; i < nonceSize; i++) {
+       char_pos= Math.floor(Math.random() * nonce_chars_length);
+       result[i]=  chars[char_pos];
+   }
+   return result.join('');
+};
+
+exports.fixedEncodeURIComponent = (text) => {
+    return encodeURIComponent(text).replace( /[\!\*\'\(\)]/g, (chr) => chr.charCodeAt(0).toString(16));
+};
 
 exports.sortObject = (OrigObject) => {
     let a = [ ];
@@ -28,20 +38,22 @@ exports.sortObject = (OrigObject) => {
     }
     
     return sorted;
-}
+};
 
 exports.stringfiller = (method, options) => {
     if(method === 'param') {
         let string = '';
         Object.entries(options).forEach(([key, value]) => {
-            string += `${exports.fixedEncodeURIComponent(key)}=${exports.fixedEncodeURIComponent(value)}&`
+            string += `${exports.fixedEncodeURIComponent(key)}=${exports.fixedEncodeURIComponent(value)}&`;
         });
         return string.slice(0, -1);
     } else if(method === 'oauthheader') {
         let string = 'Oauth ';
         Object.entries(options).forEach(([key, value]) => {
-            string += `${exports.fixedEncodeURIComponent(key)}="${exports.fixedEncodeURIComponent(value)}", `
+            if(key.startsWith('oauth_')) {
+                string += `${exports.fixedEncodeURIComponent(key)}="${exports.fixedEncodeURIComponent(value)}",`;
+            }
         });
-        return string.slice(0, -2);
+        return string;
     }
-}
+};
